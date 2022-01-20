ca98472fb4cb44a042c79a889d2351cda9fe8940
Aaron Alden <aardalde@ut.utm.edu>
2022-01-20 11:25:57 -0600

    Aaron Alden 1/20/22: Changed to include user enterted lower and upper bounds of random number generated.

--- CW2 - Getting into Git/Program.cs
+++ CW2 - Getting into Git/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// Name: Aaron Alden
+// Date: 1/20/22
+// File: Program.cs
+/* Description: A number guessing game that prompts the user to correctly guess the number
+//that was generated. Records the number of attempts and tells the user if they are too
+high or too low. 
+*/
+
+using System;
 using System.Diagnostics;
 
 namespace ConsoleApp1
@@ -7,16 +15,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello user, I am thinking of a number between 0 and 100");
             CW2__Getting_into_Git g = new CW2__Getting_into_Git();
             g.TheGuessingGame();
             Console.ReadKey(); 
         }
         void TheGuessingGame()
         {
+            int x, y;
+            Console.WriteLine("Enter a lower bound for your random number range: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an upper bound for your random number range: ");
+            y = Convert.ToInt32(Console.ReadLine());
             Random rand = new Random();
-            int number = rand.Next(0, 100);
-            Console.WriteLine(number);
+            int number = rand.Next(x, y);
             Console.WriteLine("Guess the number now:");
             int numGuess;
             int Attempts = 0;
