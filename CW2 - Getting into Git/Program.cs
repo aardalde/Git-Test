d4a3d1559b2ceab9b942d626919e1812afff244c
Aaron Alden <aardalde@ut.utm.edu>
2022-01-20 11:11:32 -0600

    Aaron Alden 1/20/22: Rewrote program into a number guessing game

--- CW2 - Getting into Git/Program.cs
+++ CW2 - Getting into Git/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -6,12 +7,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello user, I am thinking of a number between 0 and 100");
+            CW2__Getting_into_Git g = new CW2__Getting_into_Git();
+            g.TheGuessingGame();
             Console.ReadKey(); 
         }
-        void sayGoodbye()
+        void TheGuessingGame()
         {
-            Console.WriteLine("Goodbye World!");
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+            Console.WriteLine(number);
+            Console.WriteLine("Guess the number now:");
+            int numGuess;
+            int Attempts = 0;
+            do
+            {
+                numGuess = Convert.ToInt32(Console.ReadLine());
+                if (numGuess < number)
+                {
+                    Console.WriteLine(numGuess + " is too low!");
+                    Attempts++;
+                    Console.WriteLine("Guess again: ");
+                }
+                else if (numGuess > number)
+                {
+                    Console.WriteLine(numGuess + " is too high!");
+                    Attempts++;
+                    Console.WriteLine("Guess again: ");
+                }
+            } while (numGuess != number);
+            Console.WriteLine("Correct! " + numGuess + " is the right guess. It took " + 
+                "you " + Attempts + " to guess the right number.");
         }
     }
 }
