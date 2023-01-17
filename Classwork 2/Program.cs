5d6e71c9c8f34514c4826752c97a686e8896cd53
Aaron Alden <aardalde@ut.utm.edu>
2023-01-17 14:13:55 -0600

    Started implementing the required number guessing game

--- Classwork 2/Program.cs
+++ Classwork 2/Program.cs
@@ -8,7 +8,7 @@ namespace Classwork_2
 {
     class Program
     {
-        private int steve;
+        /* private int steve;
         public void setSteve(int s)
         {
             steve = s;
@@ -16,15 +16,44 @@ namespace Classwork_2
         public int getSteve()
         {
             return steve;
-        }
+        } */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!" + 5);
+           /* Console.WriteLine("Hello, world!" + 5);
             Console.ReadKey();
             Console.WriteLine("Setting Steve to 4");
             Program p = new Program();
             p.setSteve(4);
             Console.WriteLine("Steve is: " + p.getSteve());
+            Console.ReadKey(); */
+
+            Console.WriteLine("I am thinking of a number between 0 and 100...");
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+            Console.WriteLine(number);
+            Console.WriteLine("Enter your guess: ");
+            int userGuess = Convert.ToInt32(Console.ReadLine());
+            while(userGuess != number)
+            {
+                int numGuesses = 0;
+                if(userGuess < number)
+                {
+                    Console.WriteLine("Your guess was too low");
+                    numGuesses++;
+                }
+                if (userGuess > number)
+                {
+                    Console.WriteLine("Your guess was too high");
+                    numGuesses++;
+                }
+                Console.WriteLine("Enter your guess: ");
+                else
+                {
+                    //
+                }
+            }
+       
+
             Console.ReadKey();
         }
     }
